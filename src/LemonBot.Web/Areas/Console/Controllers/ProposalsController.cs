351362cacf36ebef424f5ee66df6fbce21417295
Alberto Mori <albi.mo87@hotmail.it>
2022-06-19 00:59:13 +0200

    #55 - proposals controller definition, massive nuget packages update

---
+++ src/LemonBot.Web/Areas/Console/Controllers/ProposalsController.cs
@@ -0,0 +1,99 @@
+﻿using LemonBot.Web.Areas.Console.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LemonBot.Web.Areas.Console.Controllers;
+
+[Route("api/console/[controller]")]
+[ApiController]
+[Authorize]
+public class ProposalsController : ControllerBase
+{
+    public ProposalsControllerServices ControllerServices { get; }
+
+    public ProposalsController(ProposalsControllerServices controllerServices)
+    {
+        ControllerServices = controllerServices ?? throw new ArgumentNullException(nameof(controllerServices));
+    }
+
+    [HttpGet]
+    public IActionResult GetProposals()
+    {
+        var model = ControllerServices.GetAllProposals();
+        return Ok(model);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProposalDetail(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        var model = ControllerServices.GetProposalDetail(id);
+        if (model is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(model);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> AcceptProposal(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        try
+        {
+            await ControllerServices.AcceptProposal(id);
+            return Ok();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProposal(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        try
+        {
+            await ControllerServices.DeleteProposal(id);
+            return Ok();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id}/refuse")]
+    public async Task<IActionResult> RefuseProposal(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        try
+        {
+            await ControllerServices.RefuseProposal(id);
+            return Ok();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ src/LemonBot.Web/Areas/Console/Services/ProposalsControllerServices.cs
@@ -0,0 +1,64 @@
+﻿using KITT.Core.Commands;
+using KITT.Core.ReadModels;
+using KITT.Web.Models.Proposals;
+
+namespace LemonBot.Web.Areas.Console.Services;
+
+public class ProposalsControllerServices
+{
+    public IDatabase Database { get; }
+    public IProposalCommands Commands { get; }
+
+    public ProposalsControllerServices(IDatabase database, IProposalCommands commands)
+    {
+        Database = database ?? throw new ArgumentNullException(nameof(database));
+        Commands = commands ?? throw new ArgumentNullException(nameof(commands));
+    }
+
+    public ProposalListModel GetAllProposals()
+    {
+        var proposalsQuery = Database.Proposals;
+
+        var proposals = proposalsQuery
+            .OrderBy(p => p.SubmittedAt)
+            .Select(p => new ProposalListModel.ProposalListItemModel
+            {
+                Id = p.Id,
+                AuthorNickname = p.AuthorNickname,
+                Description = p.Description,
+                Title = p.Title,
+                Status = (ProposalStatus)p.Status,
+                SubmittedAt = p.SubmittedAt
+            }).ToArray();
+
+        var model = new ProposalListModel { TotalItems = proposalsQuery.Count(), Items = proposals };
+        return model;
+    }
+
+    public ProposalDetailModel? GetProposalDetail(Guid proposalId)
+    {
+        var proposal = Database.Proposals.SingleOrDefault(p => p.Id == proposalId);
+        if (proposal == null)
+        {
+            return null;
+        }
+
+        var model = new ProposalDetailModel
+        {
+            Id = proposal.Id,
+            AuthorNickname = proposal.AuthorNickname,
+            Description = proposal.Description,
+            Status = (ProposalStatus)proposal.Status,
+            SubmittedAt = proposal.SubmittedAt,
+            Title = proposal.Title
+        };
+
+        return model;
+    }
+
+    public Task AcceptProposal(Guid proposalId) => Commands.Accept(proposalId);
+
+    public Task DeleteProposal(Guid proposalId) => Commands.Delete(proposalId);
+
+    public Task RefuseProposal(Guid proposalId) => Commands.Refuse(proposalId);
+}
