45b7985ea3626922c0671e2d413a11e50d06b00c
Alberto Mori <albi.mo87@hotmail.it>
2023-03-28 22:36:25 +0200

    created model and migration

--- src/KITT.Core/Persistence/KittDbContext.cs
+++ src/KITT.Core/Persistence/KittDbContext.cs
@@ -19,6 +19,8 @@ public class KittDbContext : DbContext
 
     public DbSet<StreamingStats> StreamingStats { get; set; }
 
+    public DbSet<Rating> Ratings { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
