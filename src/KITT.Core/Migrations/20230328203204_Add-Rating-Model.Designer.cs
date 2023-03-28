2a07d2f9dca10d925d6c44b93be7b630684db5f3
Alberto Mori <albi.mo87@hotmail.it>
2023-03-28 22:36:25 +0200

    created model and migration

--- src/KITT.Core/Migrations/20230328203204_Add-Rating-Model.Designer.cs
+++ src/KITT.Core/Migrations/20230328203204_Add-Rating-Model.Designer.cs
@@ -4,6 +4,7 @@ using KITT.Core.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KITT.Core.Migrations
 {
     [DbContext(typeof(KittDbContext))]
-    partial class KittDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230328203204_Add-Rating-Model")]
+    partial class AddRatingModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/KITT.Core/Migrations/20230328203204_Add-Rating-Model.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KITT.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_KITT_Streamings_KITT_Contents_Id",
+                table: "KITT_Streamings");
+
+            migrationBuilder.CreateTable(
+                name: "KITT_Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    PageUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    NumberOfLikes = table.Column<int>(type: "int", nullable: false),
+                    NumberOfDislikes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KITT_Ratings", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_KITT_Streamings_KITT_Contents_Id",
+                table: "KITT_Streamings",
+                column: "Id",
+                principalTable: "KITT_Contents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_KITT_Streamings_KITT_Contents_Id",
+                table: "KITT_Streamings");
+
+            migrationBuilder.DropTable(
+                name: "KITT_Ratings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_KITT_Streamings_KITT_Contents_Id",
+                table: "KITT_Streamings",
+                column: "Id",
+                principalTable: "KITT_Contents",
+                principalColumn: "Id");
+        }
+    }
+}
