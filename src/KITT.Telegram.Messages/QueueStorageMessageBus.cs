b27d1bbd5c2f2d7c397e72406e888c87d8b35c29
Alberto Mori <albi.mo87@hotmail.it>
2023-07-29 14:55:00 +0200

    #87 - logger on messagebus

--- src/KITT.Telegram.Messages/QueueStorageMessageBus.cs
+++ src/KITT.Telegram.Messages/QueueStorageMessageBus.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -7,25 +8,34 @@ namespace KITT.Telegram.Messages;
 public class QueueStorageMessageBus : IMessageBus
 {
     private readonly MessageBusOptions _options;
+    private readonly ILogger<QueueStorageMessageBus> _logger;
 
-    public QueueStorageMessageBus(IOptions<MessageBusOptions> options)
+    public QueueStorageMessageBus(IOptions<MessageBusOptions> options, ILogger<QueueStorageMessageBus> logger)
     {
         _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     public async Task SendAsync<TMessage>(TMessage message)
         where TMessage : class
     {
-        var queueName = message.GetType().GetCustomAttribute<QueueNameAttribute>()!.Name;
+        try
+        {
+            var queueName = message.GetType().GetCustomAttribute<QueueNameAttribute>()!.Name;
 
-        var client = new QueueClient(
-            _options.ConnectionString,
-            queueName,
-            new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 });
+            var client = new QueueClient(
+                _options.ConnectionString,
+                queueName,
+                new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 });
 
-        await client.CreateIfNotExistsAsync();
+            await client.CreateIfNotExistsAsync();
 
-        var data = new BinaryData(message);
-        await client.SendMessageAsync(data);
+            var data = new BinaryData(message);
+            await client.SendMessageAsync(data);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error sending message {@Message}: {ErrorMessage}", message, ex.Message);
+        }
     }
 }
