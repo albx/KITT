89f46d3be81cc22d79af9460db778ac4136c53d9
Alberto Mori <albi.mo87@hotmail.it>
2023-08-07 23:08:17 +0200

    #87 - fix integration tests, add telegram deploy action

--- tests/LemonBot.Web.Test.Integration/Fixtures/KittWebApplicationFactory.cs
+++ tests/LemonBot.Web.Test.Integration/Fixtures/KittWebApplicationFactory.cs
@@ -1,18 +1,34 @@
 ﻿using KITT.Core.Persistence;
+using KITT.Telegram.Messages;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace LemonBot.Web.Test.Integration.Fixtures;
 
 public class KittWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private Mock<IMessageBus> _messageBusMock = new Mock<IMessageBus>();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
             RemoveDbContextDescriptor(services);
             services.AddDbContext<KittDbContext>(options => options.UseInMemoryDatabase("Kitt-InMemory-Test"));
+
+            RemoveMessageBusDescriptor(services);
+            services.AddSingleton(_messageBusMock.Object);
         });
+    }
+
+    private void RemoveMessageBusDescriptor(IServiceCollection services)
+    {
+        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IMessageBus));
+        if (descriptor is not null)
+        {
+            services.Remove(descriptor);
+        }
     }
 
     private void RemoveDbContextDescriptor(IServiceCollection services)
@@ -20,7 +36,7 @@ public class KittWebApplicationFactory : WebApplicationFactory<Program>
         var descriptor = services.SingleOrDefault(
             d => d.ServiceType == typeof(DbContextOptions<KittDbContext>));
 
-        if (descriptor != null)
+        if (descriptor is not null)
         {
             services.Remove(descriptor);
         }
