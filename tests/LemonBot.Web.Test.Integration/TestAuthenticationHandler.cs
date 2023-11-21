aaf979544067c03be15ece1ffd705bc7768adffa
Alberto Mori <albi.mo87@hotmail.it>
2023-11-21 21:49:21 +0100

    #100 - fix warnings

--- tests/LemonBot.Web.Test.Integration/TestAuthenticationHandler.cs
+++ tests/LemonBot.Web.Test.Integration/TestAuthenticationHandler.cs
@@ -10,10 +10,9 @@ public class TestAuthenticationHandler : AuthenticationHandler<AuthenticationSch
     public static string UserId { get; } = Guid.NewGuid().ToString();
 
     public TestAuthenticationHandler(
-        IOptionsMonitor<AuthenticationSchemeOptions> options, 
-        ILoggerFactory logger, 
-        UrlEncoder encoder, 
-        ISystemClock clock) : base(options, logger, encoder, clock)
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder) : base(options, logger, encoder)
     {
     }
 
