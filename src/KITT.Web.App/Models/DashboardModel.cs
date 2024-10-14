464ba52f3e7c165c35ee24ae2e365c3d9bebcad5
Alberto Mori <albi.mo87@hotmail.it>
2024-10-14 22:29:01 +0200

    #109 - new webassembly app

---
+++ src/KITT.Web.App/Models/DashboardModel.cs
@@ -0,0 +1,16 @@
+﻿namespace KITT.Web.App.Models;
+
+public class DashboardModel
+{
+    public StreamingStatsDescriptor? StreamingStats { get; init; }
+
+    public ProposalStatsDescriptor? ProposalStats { get; init; }
+
+    public record StreamingStatsDescriptor(
+        int NumberOfStreamingsDelivered,
+        int NumberOfStreamingsScheduled);
+
+    public record ProposalStatsDescriptor(
+        int TotalNumberOfProposals,
+        int NumberOfProposalAccepted);
+}
