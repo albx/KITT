6649ed72d072088ca3088854f326e9f3a17c7947
albx <albi.mo87@hotmail.it>
2024-11-02 16:57:38 +0100

    #109 - CMS web app project

---
+++ src/KITT.Cms.Web.App/Components/ContentForm.razor.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace KITT.Cms.Web.App.Components;
+
+public partial class ContentForm<TContent>
+{
+    [Parameter]
+    public TContent Model { get; set; } = default!;
+
+    [Parameter]
+    public string ContentTabTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public bool ReadOnly { get; set; } = false;
+
+    [Parameter]
+    public RenderFragment<TContent> ContentInfo { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback<TContent> OnSave { get; set; }
+
+    [Parameter]
+    public EventCallback OnCancel { get; set; }
+
+    [Inject]
+    public IDialogService DialogService { get; set; } = default!;
+
+    private EditContext editContext = default!;
+
+    private bool saving = false;
+
+    protected override void OnParametersSet()
+    {
+        editContext = new EditContext(Model);
+    }
+
+    private async Task SubmitAsync()
+    {
+        saving = true;
+
+        try
+        {
+            if (!editContext.Validate())
+            {
+                return;
+            }
+
+            await OnSave.InvokeAsync(Model);
+        }
+        finally
+        {
+            saving = false;
+        }
+    }
+
+    private async Task CancelAsync() => await OnCancel.InvokeAsync();
+
+    private async Task HandleNavigationAsync(LocationChangingContext context)
+    {
+        if ((editContext?.IsModified() ?? false) && !saving)
+        {
+            string confirmText = Localizer[nameof(KITT.Web.App.UI.Resources.Common.UnsavedChangesDialogContent)];
+
+            var confirmDialog = await DialogService.ShowConfirmationAsync(
+                confirmText,
+                primaryText: Localizer[nameof(KITT.Web.App.UI.Resources.Common.Confirm)],
+                secondaryText: Localizer[nameof(KITT.Web.App.UI.Resources.Common.Cancel)],
+                title: Localizer[nameof(KITT.Web.App.UI.Resources.Common.UnsavedChangesDialogTitle)]);
+
+            var confirmResult = await confirmDialog.Result;
+
+            if (confirmResult.Cancelled)
+            {
+                context.PreventNavigation();
+            }
+        }
+    }
+}
+
