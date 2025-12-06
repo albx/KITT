9b94d4e73d340f45c821bc058526cad542910871
albx <albi.mo87@hotmail.it>
2025-12-06 18:25:22 +0100

    #124 - setup UI for connected channels list

--- src/KITT.Cms.Web.App/Pages/Settings/Channels.razor.cs
+++ src/KITT.Cms.Web.App/Pages/Settings/Channels.razor.cs
@@ -1,6 +1,16 @@
+using KITT.Cms.Web.Models.Settings;
+
 namespace KITT.Cms.Web.App.Pages.Settings;
 
 public partial class Channels
 {
+    private List<ChannelModel> channels = [];
 
+    protected override void OnInitialized()
+    {
+        channels = [
+            new ChannelModel { Id = Guid.NewGuid(), Name = "Twitch - albx87", Url = "https://www.twitch.tv/albx87" },
+            new ChannelModel { Id = Guid.NewGuid(), Name = "YouTube - albx87", Url = "https://www.youtube.com/@albx87" },
+        ];
+    }
 }
