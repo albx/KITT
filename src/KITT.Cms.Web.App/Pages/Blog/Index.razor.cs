93025a83325465c523b7b23504e5c7d35cb72d1b
albx <albi.mo87@hotmail.it>
2026-09-08 15:20:05 +0200

    #130 - add search and pagination

--- src/KITT.Cms.Web.App/Pages/Blog/Index.razor.cs
+++ src/KITT.Cms.Web.App/Pages/Blog/Index.razor.cs
@@ -1,7 +1,9 @@
 using KITT.Cms.Web.App.Clients;
+using KITT.Cms.Web.Models;
 using KITT.Cms.Web.Models.BlogPosts;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
+using PostSortDirection = KITT.Cms.Web.Models.SortDirection;
 
 namespace KITT.Cms.Web.App.Pages.Blog;
 
@@ -22,19 +24,33 @@ public partial class Index(
 
     private bool loading = false;
 
+    private Option<PostSortDirection>[] directions = [];
+
+    private Option<ContentStatus?>[] statuses = [
+        new() { Value = null, Text = "All" },
+        new() { Value = ContentStatus.Draft, Text = "Drafts only" },
+        new() { Value = ContentStatus.Published, Text = "Published only" }
+    ];
+
+    private Option<int>[] sizes = [
+        new() { Value = 5, Text = "5" },
+        new() { Value = 10, Text = "10" },
+        new() { Value = 25, Text = "25" },
+        new() { Value = 50, Text = "50" }
+    ];
+
     private PaginationState paginationState = new();
 
     protected override void OnInitialized()
     {
+        directions = Enum.GetValues<PostSortDirection>()
+            .Select(v => new Option<PostSortDirection>() { Value = v, Text = v.ToString() })
+            .ToArray();
+
         SetPaginationState();
     }
 
-    protected override async Task OnInitializedAsync()
-    {
-        await LoadPostsAsync();
-    }
-
-    private async Task LoadPostsAsync()
+    private async Task LoadPostsAsync(BlogPostsQueryModel query)
     {
         loading = true;
 
@@ -50,15 +66,21 @@ public partial class Index(
         }
     }
 
+    private async Task SearchAsync() => await LoadPostsAsync(query);
+
+    private async Task OnPageChangedAsync(int pageIndex)
+    {
+        query.Page = pageIndex + 1;
+        await LoadPostsAsync(query);
+    }
+
     private void SetPaginationState()
     {
         paginationState.ItemsPerPage = query.Size;
     }
 
-    private void OpenPostDetailPage(BlogPostListModel.BlogPostListItemModel post)
-    {
-        navigationManager.NavigateTo($"/blog/post/{post.Id}");
-    }
+    private void OpenPostDetailPage(BlogPostListModel.BlogPostListItemModel post) 
+        => navigationManager.NavigateTo($"/blog/post/{post.Id}");
 
     private async Task DeletePostAsync(BlogPostListModel.BlogPostListItemModel post)
     {
@@ -82,7 +104,15 @@ public partial class Index(
             }
 
             toastService.ShowSuccess($"Post {postTitle} deleted successfully!");
-            await LoadPostsAsync();
+            await LoadPostsAsync(query);
         }
+    }
+
+    private async Task ClearSearchAsync()
+    {
+        query = new();
+        SetPaginationState();
+
+        await LoadPostsAsync(query);
     }
 }
